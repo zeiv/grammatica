145c53d506359e4a732d77b874317ded73263601
zeiv <fxb9500@gmail.com>
2013-01-31 15:01:20 -0600

    Begin subject class

--- lib/grammatica.rb
+++ lib/grammatica.rb
@@ -1,5 +1,10 @@
 require "grammatica/version"
 
+require "grammatica/sentence"
+require "grammatica/subject"
+require "grammatica/verb"
+require "grammatica/object"
+
 module Grammatica
   # Your code goes here...
 end
